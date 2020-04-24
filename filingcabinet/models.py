66d5b490e482690bef3d411bd2010a3c41db4a77
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-04-24 10:07:24 +0200

    Rename ugettext_lazy to gettext_lazy

--- filingcabinet/models.py
+++ filingcabinet/models.py
@@ -9,7 +9,7 @@ from django.db import models
 from django.core.files.base import File
 from django.urls import reverse
 from django.utils.text import slugify
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.db import transaction
 
