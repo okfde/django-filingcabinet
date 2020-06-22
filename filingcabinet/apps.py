9e5eb3995fcc738ca949e2daf119bfd7ebcc526b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-06-22 13:54:03 +0200

    Remove document created signal
    Document processing should be triggered explicitly

--- filingcabinet/apps.py
+++ filingcabinet/apps.py
@@ -5,6 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class FilingCabinetConfig(AppConfig):
     name = 'filingcabinet'
     verbose_name = _('Filing Cabinet')
-
-    def ready(self):
-        import .signals  # noqa
