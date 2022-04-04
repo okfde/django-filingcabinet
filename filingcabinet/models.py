4e5b1cc12a68aeee9e42a4ccae74f07f62a50f9c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-04-04 15:58:14 +0200

    Shorten index name on document content hash

--- filingcabinet/models.py
+++ filingcabinet/models.py
@@ -223,7 +223,7 @@ class AbstractDocument(models.Model):
         indexes = [
             models.Index(
                 fields=['content_hash'],
-                name='%(app_label)s_%(class)s_chash_idx',
+                name='fc_document_chash_idx',
                 condition=models.Q(content_hash__isnull=False)
             )
         ]
