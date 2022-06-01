6426f98133bc0f2d2c0fea8ba1e872533d23caf2
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-06-01 13:11:18 +0200

    Add dependency install information

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A Django app that manages documents with pages, annotations and collections.
 # @okfde/filingcabinet
 
 Includes `@okfde/filingcabinet` JS package with Vue components.
+
+
+## Dependencies
+
+Please use/review the accompanying `default.nix` Nix configuration.
---
+++ default.nix
@@ -0,0 +1,35 @@
+with import <nixpkgs> { };
+let
+  ld_packages = [
+    imagemagick
+    libcxx
+    hdf5
+    blas
+    qpdf
+  ];
+in
+pkgs.mkShell {
+  name = "fcdocs";
+  shellHook = ''
+    export LD_LIBRARY_PATH=${builtins.concatStringsSep ":" (map (x: x + "/lib") ld_packages)}:$LD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DYLD_LIBRARY_PATH
+    export HDF5_DIR=${hdf5.dev}
+  '';
+  buildInputs = [
+    python38Packages.python
+    python38Packages.setuptools
+    python38Packages.numexpr
+    python38Packages.pikepdf
+    python38Packages.numpy
+    imagemagick
+    pkg-config
+    hdf5
+    hdf5.dev
+    tesseract
+    poppler_utils
+    blas.dev
+    qpdf
+    libcxx.dev
+    rustc
+  ];
+}
