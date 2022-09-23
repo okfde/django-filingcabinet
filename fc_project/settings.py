9b9ba5d66a6eac60202114af63922bb52b982208
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-23 16:25:55 +0200

    Add basic _frontend.html template

--- fc_project/settings.py
+++ fc_project/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "APP_DIRS": True,
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "fc_project/templates"],
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
