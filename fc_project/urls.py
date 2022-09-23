62c5d389180068455b5e77eb3df317429995aad9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-23 16:40:23 +0200

    Hookup fcdocs-annotate URLs if present

--- fc_project/urls.py
+++ fc_project/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
+try:
+    from fcdocs_annotate.annotation.api import FeatureViewSet
+except ImportError:
+    FeatureViewSet = None
 from rest_framework.routers import DefaultRouter
 
 from filingcabinet.api_views import (
@@ -21,9 +25,23 @@ api_router.register(
 )
 api_router.register(r"page", PageViewSet, basename="page")
 api_router.register(r"pageannotation", PageAnnotationViewSet, basename="pageannotation")
+if FeatureViewSet:
+    api_router.register(r"feature", FeatureViewSet, basename="feature")
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("documents/", include("filingcabinet.urls")),
-    path("api/", include((api_router.urls, "api"))),
-]
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path(
+            "documents/",
+        ),
+    ]
+    + (
+        [path("documents/features/", include("fcdocs_annotate.urls"))]
+        if FeatureViewSet
+        else []
+    )
+    + [
+        path("api/", include((api_router.urls, "api"))),
+    ]
+)
