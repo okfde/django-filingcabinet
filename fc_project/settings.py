4e98519435a55cb2bc31d2da8366f089bb8b6744
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-26 13:18:11 +0200

    Set media root to data dir

--- fc_project/settings.py
+++ fc_project/settings.py
@@ -83,7 +83,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = env("MEDIA_ROOT", BASE_DIR / "media")
+MEDIA_ROOT = env("MEDIA_ROOT", BASE_DIR / "data")
 
 # Random secret key
 
