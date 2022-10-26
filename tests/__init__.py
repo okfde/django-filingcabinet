78d8c2109dae81eb6868e59737db2e2d7516f00e
Karl Engelhardt <karl.engelhardt@okfn.de>
2022-10-26 17:10:47 +0200

    ✅ Add test for document collection pagination behavious

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import slugify
 import factory
 
 from filingcabinet import get_document_model, get_documentcollection_model
+from filingcabinet.models import CollectionDirectory
 
 Document = get_document_model()
 DocumentCollection = get_documentcollection_model()
@@ -12,6 +13,8 @@ DocumentCollection = get_documentcollection_model()
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = settings.AUTH_USER_MODEL
+
+    username = factory.Sequence(lambda n: "user_%s" % n)
 
 
 class DocumentFactory(factory.django.DjangoModelFactory):
@@ -29,3 +32,15 @@ class DocumentCollectionFactory(factory.django.DjangoModelFactory):
     title = factory.Sequence(lambda n: "DocumentCollection {}".format(n))
     slug = factory.LazyAttribute(lambda o: slugify(o.title))
     user = factory.SubFactory(UserFactory)
+
+
+class CollectionDirectoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CollectionDirectory
+
+    user = factory.SubFactory(UserFactory)
+    collection = factory.SubFactory(
+        DocumentCollectionFactory, user=factory.SelfAttribute("..user")
+    )
+    depth = 1
+    name = factory.Sequence(lambda n: "Document collection %s" % n)
