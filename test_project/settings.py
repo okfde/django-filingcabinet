560a0708fdb79a403edbe39d161a97d1a6a7010f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-10-31 10:08:50 +0000

    Add celery app to test project

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,6 +1,8 @@
 import random
 from pathlib import Path
 
+from fc_project.celery import app  # noqa
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
