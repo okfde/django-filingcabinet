4bd458b687ae9b19f8871cd5c6ac462df689b272
krmax44 <hi@krmax44.de>
2022-12-05 17:52:29 +0100

    🏗️ rename `get_pdf_viewer` to `get_pdf_viewer_url`

--- filingcabinet/templatetags/filingcabinet.py
+++ filingcabinet/templatetags/filingcabinet.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_pdf_viewer(url, page_number=None):
+def get_pdf_viewer_url(url, page_number=None):
     # Use native PDF viewer in iframe
     return url
