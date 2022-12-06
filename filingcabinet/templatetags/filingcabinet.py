24ac47292185a740d7c6cbd97960a1a58bc465aa
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-12-06 15:24:00 +0100

    Use better default for page number

--- filingcabinet/templatetags/filingcabinet.py
+++ filingcabinet/templatetags/filingcabinet.py
@@ -8,7 +8,7 @@ Document = get_document_model()
 
 
 @register.inclusion_tag("filingcabinet/_document_viewer.html", takes_context=True)
-def get_pdf_viewer(context, document: Document, page_number=None):
+def get_pdf_viewer(context, document: Document, page_number=1):
     request = context["request"]
     document_context = get_document_viewer_context(
         document, request, page_number=page_number
