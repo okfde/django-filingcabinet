453bde61e16195ed72ba1afaca2a0d6323cd57d4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-12-06 14:21:22 +0100

    Fix filingcabinet document viewer template tag

--- filingcabinet/templatetags/filingcabinet.py
+++ filingcabinet/templatetags/filingcabinet.py
@@ -1,19 +1,17 @@
 from django import template
 
+from .. import get_document_model
 from ..views import get_document_viewer_context
 
 register = template.Library()
-
-
-@register.simple_tag
-def get_pdf_viewer_url(url, page_number=None):
-    # Use native PDF viewer in iframe
-    return url
+Document = get_document_model()
 
 
 @register.inclusion_tag("filingcabinet/_document_viewer.html", takes_context=True)
-def get_pdf_viewer(context, document):
+def get_pdf_viewer(context, document: Document, page_number=None):
     request = context["request"]
-    document_context = get_document_viewer_context(document, request)
+    document_context = get_document_viewer_context(
+        document, request, page_number=page_number
+    )
     context.update(document_context)
     return context
