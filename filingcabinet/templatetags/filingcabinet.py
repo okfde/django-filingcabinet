0ff2254bfe8aa98749fd5e42a818c51a32eeac7b
Max <hi@krmax44.de>
2022-12-06 14:38:56 +0100

    Render doc viewer via template tag (#30)
    * 🏗️ rename `get_pdf_viewer` to `get_pdf_viewer_url`
    
    * ✨ add doc viewer template tag
    
    * Fix filingcabinet document viewer template tag
    
    Co-authored-by: Stefan Wehrmeyer <mail@stefanwehrmeyer.com>

--- filingcabinet/templatetags/filingcabinet.py
+++ filingcabinet/templatetags/filingcabinet.py
@@ -1,9 +1,17 @@
 from django import template
 
+from .. import get_document_model
+from ..views import get_document_viewer_context
+
 register = template.Library()
+Document = get_document_model()
 
 
-@register.simple_tag
-def get_pdf_viewer(url, page_number=None):
-    # Use native PDF viewer in iframe
-    return url
+@register.inclusion_tag("filingcabinet/_document_viewer.html", takes_context=True)
+def get_pdf_viewer(context, document: Document, page_number=None):
+    request = context["request"]
+    document_context = get_document_viewer_context(
+        document, request, page_number=page_number
+    )
+    context.update(document_context)
+    return context
