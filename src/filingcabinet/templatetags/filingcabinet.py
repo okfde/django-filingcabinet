fcc8840acb5cc7808db04caeab9b013893ed2711
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-01-05 12:19:18 +0100

    Add filingcabinet template tag for document list embedding

--- src/filingcabinet/templatetags/filingcabinet.py
+++ src/filingcabinet/templatetags/filingcabinet.py
@@ -1,7 +1,9 @@
+import json
+
 from django import template
 
 from .. import get_document_model
-from ..views import get_document_viewer_context
+from ..views import get_document_viewer_context, get_js_config
 
 register = template.Library()
 Document = get_document_model()
@@ -14,4 +16,19 @@ def get_pdf_viewer(context, document: Document, page_number=1):
         document, request, page_number=page_number
     )
     context.update(document_context)
+    return context
+
+
+@register.inclusion_tag(
+    "filingcabinet/_documentcollection_viewer.html", takes_context=True
+)
+def get_document_list_viewer(context, collection_data):
+    request = context["request"]
+
+    context.update(
+        {
+            "documentcollection_data": json.dumps(collection_data),
+            "config": json.dumps(get_js_config(request)),
+        }
+    )
     return context
