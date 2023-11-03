0a88137c8a6d3f4f01a77542fb8e1a64cf437103
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-11-03 15:18:50 +0100

    Fix typo in property access

--- frontend/javascript/components/document-toolbar.vue
+++ frontend/javascript/components/document-toolbar.vue
@@ -64,7 +64,7 @@
           <span class="visually-hidden">{{ i18n.showText }}</span>
         </button>
         <a
-          v-if="!document.properties.hide_download"
+          v-if="!document.properties._hide_download"
           :href="document.file_url"
           rel="noopener"
           class="btn btn-sm btn-secondary"
