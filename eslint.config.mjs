8749f0aa10361a59dba56f0be917c054799e68a1
krmax44 <hi@krmax44.de>
2025-04-28 17:22:28 +0200

    🔧 upgrade eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,2 @@
-import prettier from 'eslint-config-prettier'
-import pluginVue from 'eslint-plugin-vue'
-import js from '@eslint/js'
-
-export default [
-  {
-    ignores: ['node_modules', '**/static', '**/build']
-  },
-  js.configs.recommended,
-  ...pluginVue.configs['flat/strongly-recommended'],
-  prettier
-]
+import config from '@okfde/eslint-config-froide/eslint.config.mjs'
+export default config
